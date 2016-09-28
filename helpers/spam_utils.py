0503f4bd294a6db3ea271e1c56d8e96facc9b49d
Maronato <gustavomaronato@gmail.com>
2016-09-28 00:43:53 -0400

    Fixed minimum spam reaction quota

--- helpers/spam_utils.py
+++ helpers/spam_utils.py
@@ -21,7 +21,7 @@ def get_spam_list():
     spam_list = []
 
     for post in data['data']:
-        if post['reactions']['summary']['total_count'] >= 0:
+        if post['reactions']['summary']['total_count'] >= 3:
             spam_list.append(post['id'])
     return spam_list
 
