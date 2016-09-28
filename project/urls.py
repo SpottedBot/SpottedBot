a8839c0414f5d06fc1ca6141c99eabb9c9441546
Maronato <gustavomaronato@gmail.com>
2016-09-27 22:08:18 -0400

    Created humans.txt and robots.txt

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 from app import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -17,5 +18,7 @@ urlpatterns = [
     url(r'^unspam_spotted/$', views.unspam_spotted),
     url(r'^mod/$', views.moderation),
     url(r'^profile/$', views.profile),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
 
 ]
