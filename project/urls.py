aa9b3a66cd90f4da005558d5db1b663ceaa5290e
Maronato <gustavomaronato@gmail.com>
2017-02-08 15:19:16 -0200

    added robots.txt and humans.txt

--- project/urls.py
+++ project/urls.py
@@ -15,17 +15,8 @@ urlpatterns = [
     url(r'^spotted/', include('spotteds.urls', namespace='spotteds')),
     url(r'^mod/', include('moderation.urls', namespace='mod')),
     url(r'^api/', include('api.urls', namespace='api')),
-    # url(r'^$', views.index, name='index'),
-    # url(r'^logout/$', views.logout),
 
-    # url(r'^post_spotted/$', views.post_spotted, name='post_spotted'),
-    # url(r'^delete_spotted/$', views.delete_spotted),
-    # url(r'^dismiss_spotted/$', views.dismiss_spotted),
-    # url(r'^report_spotted/$', views.report_spotted),
-    # url(r'^unspam_spotted/$', views.unspam_spotted),
-    # url(r'^mod/$', views.moderation),
-    # url(r'^profile/$', views.profile),
-    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    url(r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='main/robots.txt', content_type='text/plain')),
+    url(r'^humans\.txt$', TemplateView.as_view(template_name='main/humans.txt', content_type='text/plain')),
 
 ]
