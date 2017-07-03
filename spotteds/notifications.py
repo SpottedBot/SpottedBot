b5f423bfb0991f4ee8ff396db822486176cb117a
Maronato <gustavomaronato@gmail.com>
2017-07-03 14:59:38 -0300

    fixed issue #3

--- spotteds/notifications.py
+++ spotteds/notifications.py
@@ -9,7 +9,7 @@ def author_notification(instance):
     """
 
     template = "Um de seus Spotteds acaba de ser postado!"
-    href = reverse('spotteds:view_spotted', kwargs={'spottedid': instance.id})
+    href = reverse('spotteds:view_spotted', kwargs={'spottedid': instance.spotted_count})
     userid = instance.author.facebookuser.social_id
 
     app_graph().put_object(parent_object=userid, connection_name="notifications", href=href, template=template)
@@ -27,7 +27,7 @@ def target_notification(instance):
     else:
         sender = "Alguém"
     template = sender + " acaba de enviar um Spotted pra você!"
-    href = reverse('spotteds:view_spotted', kwargs={'spottedid': instance.id})
+    href = reverse('spotteds:view_spotted', kwargs={'spottedid': instance.spotted_count})
     userid = instance.target.facebookuser.social_id
 
     app_graph().put_object(parent_object=userid, connection_name="notifications", href=href, template=template)
