0c3214d926ef4ba1dce0068b199a9bd5832eb964
Maronato <gustavomaronato@gmail.com>
2017-07-03 03:43:00 -0300

    created search.js

---
+++ main/static/main/js/spotted_submission/search.js
@@ -0,0 +1,41 @@
+$('#target_name .typeahead').typeahead(null, {
+    // Initialize typeahead
+    name: 'targets',
+    limit: 10,
+    displayKey: 'name',
+    source: targets.ttAdapter(),
+    templates: {
+        suggestion: function (data) {
+            return '<div><img style="vertical-align:middle" src="' + data.picture + '"/>  <span><b>' + data.name + '</b></span></div>';
+        },
+        empty: [
+        '<div style="color: black; text-align: center; margin: 20px;" class="empty-message">',
+        'Nenhum crush com esse nome cadastrado :(',
+        '</div>'
+        ].join('\n'),
+    }
+});
+
+// Set selected user to hidden input
+$('#target_name .typeahead').on('typeahead:selected', function (e, datum) {
+    $(".tt-menu").removeClass("tt-menu-keep-open");
+    $("#id_target_id").val(datum['id']);
+    $("#target_found_tip").text("Crush selecionado!");
+});
+
+// Manually trigger typeahead
+function manual_trigger(text) {
+    $('.typeahead').typeahead('open');
+    $('.typeahead').typeahead('val', text);
+return 0;
+};
+
+// Perform manual search using bloodhoud
+function manual_search(text) {
+    var manual_search_results;
+    function sync(datums) {
+        manual_search_results = datums;
+    }
+    targets.search(text, sync);
+    return manual_search_results;
+};
