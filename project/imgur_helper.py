2809d50be67b12642fbf722591f9f3f931f93d55
Maronato <gustavomaronato@gmail.com>
2017-07-19 02:32:55 -0300

    modified imgur exception handling

--- project/imgur_helper.py
+++ project/imgur_helper.py
@@ -1,6 +1,7 @@
 from imgurpython import ImgurClient
 from django.conf import settings
 from project.loghandler import LogHandler
+from imgurpython.helpers.error import ImgurClientError
 
 logger = LogHandler(__name__).logger
 
@@ -9,7 +10,16 @@ def upload(file_path):
     try:
         client = ImgurClient(settings.IMGUR_CLIENT, settings.IMGUR_SECRET)
         data = client.upload_from_path(file_path, anon=True)
-        return {"ok": True, "url": data['link']}
+        return_data = {"ok": True, "url": data['link']}
+    except ImgurClientError as e:
+        if "JSON decoding of response failed" in getattr(e, 'error_message', ""):
+            return_data = {"ok": False, "error": "Erro na API do Imgur. Tente outro anexo."}
+        else:
+            logger.exception("Erro na API do Imgur")
+            return_data = {"ok": False, "error": "Erro na API do Imgur"}
     except Exception as e:
         logger.exception("Erro na API do Imgur")
-        return {"ok": False, "error": "Erro na API do Imgur"}
+        return_data = {"ok": False, "error": "Erro na API do Imgur"}
+
+    finally:
+        return return_data
