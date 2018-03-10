ebe889db370f31bc95c397cda0c22033912a5f48
Gustavo Maronato <gustavomaronato@gmail.com>
2018-03-09 23:22:53 -0300

    fixed error submission for unknown errors

--- project/manual_error_report.py
+++ project/manual_error_report.py
@@ -6,7 +6,10 @@ from django.views.debug import ExceptionReporter
 def exception_email(request, e):
     exc_info = sys.exc_info()
     reporter = ExceptionReporter(request, is_email=True, *exc_info)
-    subject = e.message.replace('\n', '\\n').replace('\r', '\\r')[:989]
+    try:
+        subject = e.message.replace('\n', '\\n').replace('\r', '\\r')[:989]
+    except AttributeError:
+        subject = "Unkown Error"
     message = reporter.get_traceback_text()
     mail.mail_admins(
         subject, message, fail_silently=True,
