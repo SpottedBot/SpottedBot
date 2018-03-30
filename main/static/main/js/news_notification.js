c36f99fc42b5ec3a20af17598630b55ee863084d
Gustavo Maronato <gustavomaronato@gmail.com>
2018-03-30 09:28:02 -0300

    maintenance finished

--- main/static/main/js/news_notification.js
+++ main/static/main/js/news_notification.js
@@ -2,7 +2,7 @@
 
 var nag_id = 'db_migration_nag';
 var nag_value = true;
-var notification_is_active = true;
+var notification_is_active = false;
 var notification_text = '<b>Importante!</b> O Spotted estará em manutenção hoje de 14:20 até 17:00!';
 // <a style="color: silver" href="#" onclick="$(' + "'#notification_modal'" + ').modal(' + "'show'" +')">Mais Informações</a>
 var notification_modal_header = "Olá :)";
